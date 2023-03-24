4f6c87068245512ff92ec7aa5e61f40efb2d180a
wren <zennguyen2511@gmail.com>
2023-03-23 23:15:19 -0500

    fix html

--- script.js
+++ script.js
@@ -1,75 +1,47 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const searchForm = document.querySelector('#search-form');
-  const currentWeather = document.querySelector('#current-weather');
-  const searchHistory = document.querySelector('#search-history');
+const form = document.querySelector('form');
+const locationInput = document.querySelector('#location-input');
+const weatherInfo = document.querySelector('#weather-info');
 
-  searchForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const searchInput = document.querySelector('#search-input');
-    const cityName = searchInput.value;
-    getWeatherData(cityName);
-  });
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); // prevent form from submitting
 
-  function getWeatherData(cityName) {
-    const apiKey = 'ea081e7c8189b40b973d3d4c71f263d0';
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`;
+  const location = locationInput.value;
 
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        // Update the current weather section with the received data
-        const city = data.name;
-        const date = new Date(data.dt * 1000).toLocaleDateString();
-        const icon = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
-        const temp = Math.round(data.main.temp);
-        const humidity = data.main.humidity;
-        const wind = data.wind.speed;
-        currentWeather.innerHTML = `
-          <h2>${city} (${date}) <img src="${icon}" alt="${data.weather[0].description}" /></h2>
-          <p>Temperature: ${temp} °C</p>
-          <p>Humidity: ${humidity}%</p>
-          <p>Wind Speed: ${wind} m/s</p>
-        `;
+  // call function to fetch weather data from API
+  getWeatherData(location)
+    .then((data) => {
+      // display weather information in the weatherInfo div
+      displayWeatherInfo(data);
+    })
+    .catch((error) => {
+      console.error(error);
+      // display error message in the weatherInfo div
+      weatherInfo.innerHTML = `Error: ${error.message}`;
+    });
+});
 
-        // Fetch 5-day forecast data using the city's coordinates
-        const lat = data.coord.lat;
-        const lon = data.coord.lon;
-        const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-        fetch(forecastUrl)
-          .then(response => response.json())
-          .then(data => {
-          console.log(data); // add this line
-          // Update the 5-day forecast section with the received data
-          const forecastContainer = document.querySelector('.forecast-container');
-      
-          if (data.daily && data.daily.length >= 5) {
-            for (let i = 0; i < 5; i++) {
-              const date = new Date(data.daily[i].dt * 1000).toLocaleDateString();
-              const icon = `https://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}.png`;
-              const temp = Math.round(data.daily[i].temp.day);
-              const wind = data.daily[i].wind_speed;
-              const humidity = data.daily[i].humidity;
-              const forecastItem = document.createElement('div');
-              forecastItem.classList.add('forecast-item');
-              forecastItem.innerHTML = `
-                <p>${date} <img src="${icon}" alt="${data.daily[i].weather[0].description}" /></p>
-                <p>Temp: ${temp} °C</p>
-                <p>Wind: ${wind} m/s</p>
-                <p>Humidity: ${humidity}%</p>
-              `;
-              forecastContainer.appendChild(forecastItem);
-            }
-          } else {
-            console.log('No 5-day forecast data available'); // add this line
-          }
-          
+async function getWeatherData(location) {
+  const apikey = 'ea081e7c8189b40b973d3d4c71f263d0';
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apikey}`;
 
-          // Store the searched city in the search history section
-          const li = document.createElement('li');
-          li.textContent = cityName;
-          searchHistory.appendChild(li);
-        });
-      });                             
-    }
-  })
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error('Unable to fetch weather data');
+  }
+
+  const data = await response.json();
+
+  return data;
+}
+
+function displayWeatherInfo(data) {
+  const { name, main: { temp, humidity }, weather: [ { description } ] } = data;
+
+  weatherInfo.innerHTML = `
+    <h2>${name}</h2>
+    <p>Temperature: ${temp}°F</p>
+    <p>Humidity: ${humidity}%</p>
+    <p>Description: ${description}</p>
+  `;
+}
