96b3d01ae9ec48c06fd36adee264739853ae3364
wren <zennguyen2511@gmail.com>
2023-03-27 18:45:59 -0500

    add update 5 days forecast

--- script.js
+++ script.js
@@ -22,26 +22,51 @@ form.addEventListener('submit', (event) => {
 
 async function getWeatherData(location) {
   const apikey = 'ea081e7c8189b40b973d3d4c71f263d0';
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apikey}`;
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apikey}`;
+  const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${apikey}`;
 
-  const response = await fetch(url);
+  const [weatherResponse, forecastResponse] = await Promise.all([
+    fetch(weatherUrl),
+    fetch(forecastUrl)
+  ]);
 
-  if (!response.ok) {
+  if (!weatherResponse.ok || !forecastResponse.ok) {
     throw new Error('Unable to fetch weather data');
   }
 
-  const data = await response.json();
+  const weatherData = await weatherResponse.json();
+  const forecastData = await forecastResponse.json();
 
-  return data;
+  return { weatherData, forecastData };
 }
 
 function displayWeatherInfo(data) {
-  const { name, main: { temp, humidity }, weather: [ { description } ] } = data;
+  const { weatherData, forecastData } = data;
+  const { name, main: { temp, humidity }, weather: [ { description } ] } = weatherData;
+  const forecastList = forecastData.list.slice(0, 5);
+
+  let forecastHtml = '';
+  forecastList.forEach((forecast) => {
+    const date = new Date(forecast.dt * 1000);
+    const { main: { temp }, weather: [ { description } ] } = forecast;
+
+    forecastHtml += `
+      <div class="forecast">
+        <p>${date.toLocaleDateString()}</p>
+        <p>Temperature: ${temp}°F</p>
+        <p>Description: ${description}</p>
+      </div>
+    `;
+  });
 
   weatherInfo.innerHTML = `
     <h2>${name}</h2>
     <p>Temperature: ${temp}°F</p>
     <p>Humidity: ${humidity}%</p>
     <p>Description: ${description}</p>
+    <div class="forecast-list">
+      <h3>5-Day Forecast</h3>
+      ${forecastHtml}
+    </div>
   `;
 }
